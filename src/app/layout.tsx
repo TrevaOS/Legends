6035493ad1f1c249d096822594e75bdfe561bcce
Treva <tech@treva.in>
2026-04-23 15:34:13 +0000

    Pixel Meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -98,8 +98,31 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1343335444390830');
+fbq('track', 'PageView');`,
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1343335444390830&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
         <Navbar />
         <main>{children}</main>
         <Footer />
